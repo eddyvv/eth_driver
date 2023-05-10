8e7361f171f2347818e609e201b775185f1fc5a1
Eddy_I <1694662738@qq.com>
2023-05-10 08:57:59 +0800

    update ernic Registration Framework

--- drivers/eth/xt_roce.h
+++ drivers/eth/xt_roce.h
@@ -5,9 +5,11 @@
 #include <linux/pci.h>
 #include <linux/netdevice.h>
 
+#define XT_ROCE_ABI_VERSION	1
 
 struct xib_driver {
     unsigned char name[32];
+    u32 xt_abi_version;
     void (*add) (void);
     void (*remove) (void);
 };
